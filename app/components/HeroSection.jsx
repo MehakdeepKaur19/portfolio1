f5f534f176cb2ed555f9ed50a582067a61485531
MehakdeepKaur19 <122813568+MehakdeepKaur19@users.noreply.github.com>
2024-05-21 22:45:40 -0400

    Update HeroSection.jsx

--- app/components/HeroSection.jsx
+++ app/components/HeroSection.jsx
@@ -37,7 +37,7 @@ const HeroSection = () => {
               Hire Me
             </Link>
             <Link
-              href="https://github.com/MehakdeepKaur19/Resume/blob/main/Web%20Developer%20resume.pdf"
+              href="Resume Developer1.pdf"
               className="px-1 inline-block py-1 w-full sm:w-fit rounded-full bg-gradient-to-br from-primary-500 to-secondary-500 hover:bg-slate-800 text-white mt-3"
               >
               <span className="block bg-[#121212] hover:bg-slate-800 rounded-full px-5 py-2">
